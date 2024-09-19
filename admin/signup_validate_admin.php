c7ba98a6c60c0a07bc62e170414e94008f0e950f
donatedsalt <mmohsinkhan3685@gmail.com>
2024-09-19 11:27:49 +0500

    admin signin and signup

---
+++ admin/signup_validate_admin.php
@@ -0,0 +1,17 @@
+<?php
+include "../conn.php";
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+
+    $query = "INSERT INTO admin ( name , email , password )
+    VALUES ( '$name', '$email' , '$password' )";
+
+    $run = mysqli_query(mysql: $conn, query: $query);
+    if ($run) {
+        echo "<script>alert('Record Submitted!')</script>";
+    }
+}
