82d48b7908a044bbd42eb37ab54175269bdb03fb
bilalhassan <bilalhassan4048@gmail.com>
2024-09-19 15:25:40 +0500

    billo

---
+++ admin/drivers_profile.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include "../conn.php";
+
+include "include/head.php"; ?>
+
+<body>
+    <div class="container-fluid position-relative d-flex p-0">
+
+
+        <?php include "include/sidebar.php"; ?>
+
+        <!-- Content Start -->
+        <div class="content">
+
+            <?php include "include/navbar.php"; ?>
+
+
+            <!-- Recent Sales Start -->
+            <?php
+            if (isset($_POST['submit'])) {
+                $first_name = $_POST['first_name'];
+                $last_name = $_POST['last_name'];
+                $contact = $_POST['contact'];
+                $query = "INSERT INTO driver (first_name, last_name, contact) VALUES ('$first_name', '$last_name', '$contact')";
+                $run = mysqli_query($conn, $query);
+
+                if ($run) {
+                    echo "<script>alert('Record is submitted!')</script>";
+                }
+            }
+            ?>
+            <div class="container-fluid pt-4 px-4">
+                <form method="POST" class="mt-4">
+                    <div class="col-12">
+                        <input type="text" name="first_name" class="form-control border border-white" placeholder="Enter first name">
+                    </div>
+                    <br>
+                    <div class="col-12">
+                        <input type="text" name="last_name" class="form-control border border-white" placeholder="Enter last name">
+                    </div>
+                    <br>
+                    <div class="col-12">
+                        <input type="text" name="contact" class="form-control border border-white bg-dark" placeholder="Enter contact">
+                    </div>
+                    <br>
+                    <div class="col-3">
+                        <input type="submit" name="submit" value="Create" class="btn m-2 ab">
+                    </div>
+                </form>
+
+
+
+
+
+                <div class="bg-secondary text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h6 class="mb-0">Recent Salse</h6>
+                        <a href="">Show All</a>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table text-start align-middle table-bordered table-hover mb-0">
+                            <thead>
+                                <tr class="text-white">
+                                    <th scope="col"><input class="form-check-input" type="checkbox"></th>
+                                    <th scope="col">Driver id</th>
+                                    <th scope="col">First name</th>
+                                    <th scope="col">Last name</th>
+                                    <th scope="col">Contact</th>
+                                    <th scope="col">Action</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <?php
+                                $query = "SELECT * from driver ";
+
+                                $runn = mysqli_query($conn, $query);
+
+                                while ($rows = mysqli_fetch_array($runn)) {
+                                ?> <tr>
+                                        <td><input class="form-check-input" type="checkbox"></td>
+
+                                        <td><?php echo $rows['driver_id'] ?></td>
+                                        <td><?php echo $rows['first_name'] ?></td>
+                                        <td><?php echo $rows['last_name'] ?></td>
+                                        <td><?php echo $rows['contact'] ?></td>
+                                        <td>
+                                            <a class="btn btn-sm btn-primary" href="delete_drivers_profile.php?id=<?= $rows['driver_id'] ?>">Delete</a>
+                                            <a class="btn btn-sm btn-primary" href="edit_drivers_profile.php?id=<?= $rows['driver_id'] ?>">Edit</a>
+                                        </td>
+
+                                    </tr>
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- Recent Sales End -->
+
+
+
+
+        </div>
+        <!-- Content End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <?php include "include/scripts.php"; ?>
+
+</body>
+
+</html>
---
+++ admin/edit_drivers_profile.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include "../conn.php";
+
+ob_start();
+
+$id = $_GET['id'];
+
+$query = "SELECT * FROM driver WHERE driver_id = $id";
+
+$runn = mysqli_query($conn, $query);
+
+$data =  mysqli_fetch_array($runn);
+
+ob_end_flush();
+
+if (isset($_POST["submit"])) {
+
+    $first_name  = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $contact = $_POST['contact'];
+
+    $queryys = "UPDATE driver SET first_name = '$first_name', last_name = '$last_name', contact = '$contact' WHERE driver_id = '$id'";
+
+
+    $runn = mysqli_query($conn, $queryys);
+
+    if ($runn) {
+        echo "<script>window.location.href='drivers_profile.php'</script>";
+    }
+}
+?>
+<?php
+include "../conn.php";
+
+include "include/head.php"; ?>
+
+<body>
+    <div class="container-fluid position-relative d-flex p-0">
+
+
+        <?php include "include/sidebar.php"; ?>
+
+        <!-- Content Start -->
+        <div class="content">
+
+            <?php include "include/navbar.php"; ?>
+
+            <div class="container-fluid pt-4 px-4">
+                <form method="POST" class="mt-4">
+                    <div class="col-12">
+                        <input type="text" name="first_name" class="form-control border border-white" placeholder="Edit first name" value="<?php echo $data['first_name'] ?>">
+                    </div>
+                    <br>
+                    <div class="col-12">
+                        <input type="text" name="last_name" class="form-control border border-white" placeholder="Edit last name"
+                            value="<?php echo $data['last_name'] ?>">
+                    </div>
+                    <br>
+                    <div class="col-12">
+                        <input type="text" name="contact" class="form-control border border-white bg-dark" placeholder="Edit contact"
+                            value="<?php echo $data['contact'] ?>">
+                    </div>
+                    <br>
+                    <div class="col-3">
+                        <input type="submit" name="submit" value="Create" class="btn m-2 ab">
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        <!-- Content End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <?php include "include/scripts.php"; ?>
+
+</body>
+
+</html>
