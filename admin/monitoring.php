ffae60042de4c8365ee7787ef0d3f869c1d5eb32
bilalhassan <bilalhassan4048@gmail.com>
2024-09-19 16:47:50 +0500

    styling fixes

--- admin/monitoring.php
+++ admin/monitoring.php
@@ -15,9 +15,9 @@ include "include/head.php";
         <div class="content">
 
             <?php include "include/navbar.php"; ?>
-            <div class="container-fluid pt-4 px-4">
+            <div class="container-fluid d-flex pt-4 px-4 w-100" style="height: 90%;">
 
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14466.136769377172!2d67.0652637!3d24.9819581!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb340e584b891c3%3A0x29b2cbc198ba2dbd!2sAptech%20Computer%20Education%20North%20Karachi%20Center!5e0!3m2!1sen!2s!4v1726742618403!5m2!1sen!2s" width="1300" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14466.136769377172!2d67.0652637!3d24.9819581!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb340e584b891c3%3A0x29b2cbc198ba2dbd!2sAptech%20Computer%20Education%20North%20Karachi%20Center!5e0!3m2!1sen!2s!4v1726742618403!5m2!1sen!2s" class="flex-grow-1 rounded" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
         </div>
