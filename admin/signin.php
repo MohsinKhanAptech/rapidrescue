e20449fabc5624e9b8c6763b51d6d8bf170598a4
bilalhassan <bilalhassan4048@gmail.com>
2024-09-20 14:43:56 +0500

    commit

--- admin/signin.php
+++ admin/signin.php
@@ -32,7 +32,7 @@
                                 <a href="">Forgot Password?</a>
                             </div>
                             <input type="submit" name="submit" class="btn btn-primary py-3 w-100 mb-4" value="sign in">
-                            <p class="text-center mb-0">Don't have an Account? <a href="signup.php">Sign Up</a></p>
+                            <!-- <p class="text-center mb-0">Don't have an Account? <a href="signup.php">Sign Up</a></p> -->
                         </form>
                     </div>
                 </div>
