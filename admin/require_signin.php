dea93bdb010178a088c690bdbb9de9c6e40395eb
donatedsalt <mmohsinkhan3685@gmail.com>
2024-09-20 14:58:49 +0500

    fix admin signin

--- admin/require_signin.php
+++ admin/require_signin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if (isset($_SESSION['email'])) {
-    $email = $_SESSION['email'];
+if (isset($_SESSION['admin_email'])) {
+    $email = $_SESSION['admin_email'];
 } else {
     header('location: signin.php');
 }
