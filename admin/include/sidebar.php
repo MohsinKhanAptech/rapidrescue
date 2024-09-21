690a372686d8fd6aed22203dd58cb1b68f448f92
mohsinkhanaptech <muhammadmohsinkhan.aptech@gmail.com>
2024-09-21 15:51:46 +0500

    update

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -5,18 +5,17 @@
             <h3 class="text-primary">RapidRescue</h3>
         </a>
         <div class="navbar-nav w-100">
+            <a href="chat.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Chats</a>
+            <a href="user.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Users</a>
             <a href="ambulance.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i> Ambulances</a>
             <a href="driver.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i> Drivers</a>
             <a href="technicians.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Technicians</a>
             <a href="requests.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Requests</a>
-            <a href="dispatch.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i> Dispatch</a>
-            <a href="monitoring.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Monitoring</a>
-            <a href="user.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Users</a>
+            <!-- <a href="dispatch.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i> Dispatch</a> -->
             <a href="medical_profile.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Med profile</a>
+            <a href="monitoring.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Monitoring</a>
             <a href="contact.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Contacts</a>
             <a href="feedback.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>feedbacks</a>
-            <a href="chat.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Chats</a>
-
         </div>
     </nav>
 </div>
